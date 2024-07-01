6d4334c04d068e0fbe84936256349187a4789a98
trixiabelleza <trixiabelleza2@gmail.com>
2024-07-01 09:01:33 +0800

    Added User Registration Feature
    In this commit, we are introducing the ff changes:
      - New entities: Project, and User
      - User Repository that extends JpaRepository for db queries
      - UserService that implements userRepository for data retrieval and data storage of user-related operations.
      - UI for login and registration

---
+++ src/main/java/com/dev/timetracker/dto/UserDto.java
@@ -0,0 +1,22 @@
+package com.dev.timetracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * To transfer data between controller layer and view layer
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/dev/timetracker/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.dev.timetracker.security;
+
+import com.dev.timetracker.entity.User;
+import com.dev.timetracker.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+       User user = userRepository.findByEmail(email);
+
+       if (user != null) {
+           return org.springframework.security.core.userdetails.User.withUsername(user.getEmail())
+                   .password(user.getPassword())
+                   .authorities("USER").build();
+       } else {
+           throw new UsernameNotFoundException("Invalid username or password");
+       }
+    }
+}
